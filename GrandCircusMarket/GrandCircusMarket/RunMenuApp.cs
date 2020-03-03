1c586784bd7fe4b1e164b3a19d7c4bfbbfd57294
Tim <tpclark@umich.edu>
2020-03-02 19:18:50 -0500

    Payment types added

--- GrandCircusMarket/GrandCircusMarket/RunMenuApp.cs
+++ GrandCircusMarket/GrandCircusMarket/RunMenuApp.cs
@@ -8,10 +8,19 @@ namespace GrandCircusMarket
     {
       public void RunApp()
         {
-            
-
-            
-
+            Console.WriteLine("How would you like to pay? Please select one of the following payment types:");
+            Console.WriteLine("[1] Cash");
+            Console.WriteLine("[2] Credit or Debit Card");
+            Console.WriteLine("[3] Check");
+            var userSelectedPaymentType = int.Parse(Console.ReadLine());
+            if (userSelectedPaymentType == 1)
+            {
+                Console.WriteLine("Please enter the amount of cash tendered");
+                var userInput = double.Parse(Console.ReadLine());
+                var cashFromCustomer = new Cash();
+                cashFromCustomer.Amount = userInput;
+                Console.WriteLine($"You have given me {cashFromCustomer}");
+            }
         }
 
     }
